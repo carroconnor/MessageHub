bb61ccaded11a8862892ed29ae8bce0978a023f4
Carr OConnor <carr.oconnor@gmail.com>
2017-12-13 15:02:42 -0500

    added create form

---
+++ MessageHub/Controllers/MessagesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageHub.Models;
+using MessageHub.ViewModels;
+
+namespace MessageHub.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MessagesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Messages
+        public ActionResult Create()
+        {
+
+            var viewModel = new MessageFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ MessageHub/ViewModels/MessageFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MessageHub.Models;
+
+namespace MessageHub.ViewModels
+{
+    public class MessageFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
