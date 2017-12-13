e3e2c812a4606217b22e6bfe91f96cf283955835
Carr OConnor <carr.oconnor@gmail.com>
2017-12-13 11:40:46 -0500

    Add project files.

---
+++ MessageHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessageHub.MvcApplication" Language="C#" %>
---
+++ MessageHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageHub.Startup))]
+namespace MessageHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
