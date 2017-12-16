cdc5273983878e2ed7e6d41dae2f772556627204
Carr OConnor <carr.oconnor@gmail.com>
2017-12-16 12:20:36 -0500

    refactored

--- MessageHub/Controllers/AccountController.cs
+++ MessageHub/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MessageHub.Models;
+using MessageHub.ViewModels;
 
 namespace MessageHub.Controllers
 {
@@ -151,7 +152,7 @@ namespace MessageHub.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { Name = model.Name, UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- MessageHub/Controllers/HomeController.cs
+++ MessageHub/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using MessageHub.Models;
 
 namespace MessageHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingMessages = _context.Messages
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingMessages);
         }
 
         public ActionResult About()
